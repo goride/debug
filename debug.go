620e0d273b14c5df1e5d2a494cdbbc586aff796a
Nimo <nimo.jser@gmail.com>
2019-07-07 18:58:27 +0800

    :fire: remove server

--- debug.go
+++ debug.go
@@ -2,10 +2,9 @@ package debug
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 type Record struct {
@@ -14,34 +13,9 @@ type Record struct {
 }
 var recordList []Record
 
-func Check (err error) {
+func check (err error) {
 	if err != nil {
 		log.Fatal(err)
-	}
-}
-func htmlController(w http.ResponseWriter, r *http.Request) {
-	htmlByte, err := ioutil.ReadFile("debug.html");Check(err)
-	html := string(htmlByte)
-	w.Header().Set("Content-Type", "text/html")
-	_, err = fmt.Fprintf(w, html);Check(err)
-}
-func debugController(w http.ResponseWriter, r *http.Request) {
-	var dataVO = map[string]interface{}{
-		"type": "pass",
-		"data": recordList,
-	}
-	//log.Print(recordList)
-	dataJO, _ := json.Marshal(dataVO)
-	_, _ = fmt.Fprintf(w, string(dataJO))
-}
-func startServer () {
-	http.HandleFunc("/", htmlController)
-	http.HandleFunc("/debug", debugController)
-	err := http.ListenAndServe(":9999", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	} else {
-		log.Print("listen success")
 	}
 }
 func Debug (v ...interface{}) {
@@ -50,9 +24,21 @@ func Debug (v ...interface{}) {
 		output := map[string]interface{}{
 			"value": value,
 		}
-		jsonbyte, _ := json.Marshal(output)
+		jsonbyte, err := json.Marshal(output)
+		check(err)
 		jsonstring := string(jsonbyte)
 		log.Printf("%+v", jsonstring)
 	}
-	startServer()
+	postValueJSONbyte, err := json.Marshal(recordList)
+	check(err)
+	postValueJSON := string(postValueJSONbyte)
+	resp, err := http.Post("http://127.0.0.1:9999/debug/add",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("json=" + postValueJSON))
+	check(err)
+	defer resp.Body.Close()
+	//body, err := ioutil.ReadAll(resp.Body)
+	//check(err)
+	//log.Println(string(body))
+
 }
